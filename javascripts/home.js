31e08eef20bf273684566433ec39ae6f5ffabb87
Mike Moore <mike@blowmage.com>
2010-09-14 12:43:26 -0600

    Get Twitter tweets via JSONP

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,3 +1,19 @@
+function loadTwitterTweets(data) {
+  $('div#tweets').html('');
+  jQuery.each(data.slice(0,10), function(index, tweet){
+    $('div#tweets').append(tweetHTML(tweet));
+  });
+}
+
+function tweetHTML(tweet) {
+  var div = $('<div>', {'class': 'tweet'});
+  div.append($('<blockquote>').append(tweet.text));
+  var link = $("<a>", {'href': 'http://twitter.com/blowmage/status/' + tweet.id});
+  link.append('tweeted ' + prettyDate(tweet.created_at));
+  div.append($('<p>').append(link));
+  return div;
+}
+
 function retrieveCommits(data) {
   return jQuery.map(data, function(value, index) {
     return (value.type == 'PushEvent') ? value : null
@@ -35,4 +51,31 @@ function loadGitHubCommits(data) {
   jQuery.each(commits, function(index, commit){
     $('div#commits').append(commit);
   });
+}
+
+/*
+ * JavaScript Pretty Date
+ * Copyright (c) 2008 John Resig (jquery.com)
+ * Licensed under the MIT license.
+ */
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(time){
+  var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+    diff = (((new Date()).getTime() - date.getTime()) / 1000),
+    day_diff = Math.floor(diff / 86400);
+
+  if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+    return;
+
+  return day_diff == 0 && (
+      diff < 60 && "just now" ||
+      diff < 120 && "1 minute ago" ||
+      diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+      diff < 7200 && "1 hour ago" ||
+      diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+    day_diff == 1 && "yesterday" ||
+    day_diff < 7 && day_diff + " days ago" ||
+    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
 }
