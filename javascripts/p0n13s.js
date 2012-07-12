509d2472166e0190082cce66681cbda5a36a7614
Mike Moore <mike@blowmage.com>
2012-07-12 11:12:55 -0600

    Add random ponies

--- javascripts/p0n13s.js
+++ javascripts/p0n13s.js
@@ -1,5 +1,7 @@
 jQuery(function($) {
-  $('body').append('<img id="p0n13s" src="/images/ponies.png" style="position:fixed;bottom:0;right:0;height:300px;">');
+  var ponies = Array('hug.png', 'sunglasses.png');
+  var pony   = ponies[Math.floor(Math.random()*ponies.length)];
+  $('body').append('<img id="p0n13s" src="/images/ponies/' + pony + '" style="position:fixed;bottom:0;right:0;height:300px;">');
   $('body > div.wrapper > section').append('<div id="pwnies"></div>');
   $('#p0n13s').hide();
   $('#pwnies').waypoint(function(event, direction) {
