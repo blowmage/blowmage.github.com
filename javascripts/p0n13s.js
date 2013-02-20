163f49a46ee604eb6ae295acc4f49e00d9c6f3f8
Mike Moore <mike@blowmage.com>
2013-02-20 15:05:16 -0700

    Fix pony scrolling

--- javascripts/p0n13s.js
+++ javascripts/p0n13s.js
@@ -4,7 +4,7 @@ jQuery(function($) {
   $('body').append('<img id="p0n13s" src="/images/ponies/' + pony + '" style="position:fixed;bottom:0;right:0;height:300px;">');
   $('body > div.wrapper > section').append('<div id="pwnies"></div>');
   $('#p0n13s').hide();
-  $('#pwnies').waypoint(function(event, direction) {
+  $('#pwnies').waypoint(function(direction) {
     if (direction === "up") {
       $('#p0n13s').slideUp();
     } else {
