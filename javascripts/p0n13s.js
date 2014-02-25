0a1e8fe8c6ea3cfe311487081fc81fbf01ba3679
Mike Moore <mike@blowmage.com>
2014-02-25 16:12:30 -0700

    Switch to Lanyon theme

--- javascripts/p0n13s.js
+++ javascripts/p0n13s.js
@@ -2,7 +2,7 @@ jQuery(function($) {
   var ponies = Array('hug.png', 'sunglasses.png', 'mane6.png', 'u-mad.png', 'crash.png');
   var pony   = ponies[Math.floor(Math.random()*ponies.length)];
   $('body').append('<img id="p0n13s" src="/images/ponies/' + pony + '" style="position:fixed;bottom:0;right:0;height:300px;">');
-  $('body > div.wrapper > section').append('<div id="pwnies"></div>');
+  $('body').append('<div id="pwnies"></div>');
   $('#p0n13s').hide();
   $('#pwnies').waypoint(function(direction) {
     if (direction === "up") {
